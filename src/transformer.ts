60c0e4cb7bc5521fec1c3d58d1860b43aa15350e
Evgeniy Timokhov <timocov@gmail.com>
2019-03-24 01:47:22 +0300

    Allowed override default options

--- src/transformer.ts
+++ src/transformer.ts
@@ -2,8 +2,12 @@ import * as ts from 'typescript';
 
 import { PropertiesMinifier } from './properties-minifier';
 
+export interface PluginConfig {
+	emitOriginalName: boolean;
+}
+
 // tslint:disable-next-line:no-default-export
-export default function transformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
-	const minifier = new PropertiesMinifier({ emitOriginalName: true });
+export default function transformer(program: ts.Program, config?: Partial<PluginConfig>): ts.TransformerFactory<ts.SourceFile> {
+	const minifier = new PropertiesMinifier({ emitOriginalName: true, ...config });
 	return (context: ts.TransformationContext) => (file: ts.SourceFile) => minifier.visitSourceFile(file, program, context);
 }

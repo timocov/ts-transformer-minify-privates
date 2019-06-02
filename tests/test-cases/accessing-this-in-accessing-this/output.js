727679e2d9b35f64b7bd18b936d169b8a137cd99
Evgeniy Timokhov <timocov@gmail.com>
2019-06-02 23:42:45 +0300

    Added test case with accessing this in accessing this

---
+++ tests/test-cases/accessing-this-in-accessing-this/output.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Class = /** @class */ (function () {
+    function Class() {
+        this.pimp = {};
+        this.my = {};
+        this.ride = 'x-zibit';
+        this.xzibit = 'mtv';
+        console.log(this.pimp[this.my[this.ride]][this.xzibit]++);
+    }
+    return Class;
+}());
+exports.Class = Class;
